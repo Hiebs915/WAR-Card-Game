46c2fe2d5f0c370efa2a383ca5a9c18eb0ab5450
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-23 09:28:22 -0500

    Player Class
    Created player class which gives each player a name, removes one card at a time, and adds single or multiple cards back to the deck.

--- WAR Card Game Code.py
+++ WAR Card Game Code.py
@@ -1,8 +1,7 @@
 import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
-            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 
 
@@ -18,7 +17,7 @@ class Card:
 
 
 class Deck:
-    def __inti__(self):
+    def __init__(self):
         self.all_cards = []
 
         for suit in suits:
@@ -33,3 +32,24 @@ class Deck:
 
     def deal_one(self):
         return self.all_cards.pop()
+
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.all_cards = []
+
+        def remove_one(self):
+            return self.all_cards.pop(0)
+
+        def add_cards(self,new_cards):
+            if type(new_cards) == type([]):
+                # List of multiple card objects
+                self.all_cards.extend(new_cards)
+            else:
+                # For single card object
+                self.all_cards.append(new_cards)
+
+        def __str__(self):
+            return f'Player {self.name} has {len(self.all_cards)} cards.'
